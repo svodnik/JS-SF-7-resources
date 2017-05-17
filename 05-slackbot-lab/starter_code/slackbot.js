e772212c2943db8afbcfdb8ea50be24280802e19
svodnik <sashavodnik@gmail.com>
2017-05-17 13:37:08 -0700

    updated resources before class

--- 05-slackbot-lab/starter_code/slackbot.js
+++ 05-slackbot-lab/starter_code/slackbot.js
@@ -32,6 +32,13 @@ EXAMPLES OF THE KEY HUBOT FUNCTIONS
 
 ************************************/
 
+
+/* IMPORTANT! 
+You can have only one instance of module.exports in each JavaScript file. 
+If you want to supplement your existing code above with any the code below, 
+you need to move the contents of module.exports below into the module.exports code above. 
+*/
+
 /*
 var squirrels;
 squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
